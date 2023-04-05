45cf7af5b701f4d7f5f3d4b7b637782e015b1562
Berhanu <eyerusalemmengiste@gmail.com>
2023-04-05 14:42:24 +0300

    Add the wall feature

--- src/wall.c
+++ src/wall.c
@@ -3,7 +3,7 @@
 /**
  * changeColorIntensity - change color intensity
  * based on a factor value between 0 and 1
- * @factor: intensity factor
+ * @factor: intensity factor 
  * @color: color for intensity
 */
 
